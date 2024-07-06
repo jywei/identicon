ef3b078a975124b7163cf771856ebac82132e6fb
Roy Wei <roywjy@gmail.com>
2024-07-06 15:38:27 +1000

    Add doc comment

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,15 @@ defmodule Identicon do
     |> hash_input
   end
 
+  @doc """
+  Hashes the input using MD5 and converts it to a list of bytes.
+
+  ## Examples
+
+      iex> Identicon.hash_input("hello")
+
+      [93, 65, 64, 42, 188, 75, 42, 118, 185, 113, 157, 145, 16, 23, 197, 146]
+  """
   def hash_input(input) do
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list
