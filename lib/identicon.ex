7b87ae5aae80adc2d9d8df55ef14c49b0822eba3
Roy Wei <roywjy@gmail.com>
2024-07-06 16:22:38 +1000

    Refactor build_grid and add grid to struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,11 +10,16 @@ defmodule Identicon do
     |> build_grid
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = _image) do
-    hex
-    |> Enum.chunk_every(3)
-    |> Enum.filter(&length(&1) == 3) # Ensure each chunk has exactly 3 elements
-    |> Enum.map(&mirror_row/1)
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.filter(&length(&1) == 3) # Ensure each chunk has exactly 3 elements
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
